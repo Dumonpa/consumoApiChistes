e2a74d8c6dd9f8e53cba1700a512e6bb450b410c
Carlos <carbelez123@gmail.com>
2020-11-06 10:52:52 -0500

    inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Consumo de API
+
+Este es el proyecto donde se practica el consumo de api de chistes con JS.
+
+### Notas:
+Recuerden reconstruir los módulos de Node
+```
+npm install
+```
+
+Y para construir el build, recuerden:
+```
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,28 @@
+// import { obtenerUsuarios } from "./js/http-provider";
+// const { obtenerUsuarios } = require("./js/http-provider");
+
+// import * as CRUD from "./js/crud-provider";
+
+// obtenerUsuarios().then(console.log);
+
+// import { init } from './js/usuarios-page';
+
+// init();
+
+// CRUD.getUsuario(1).then(console.log);
+
+// CRUD.crearUsuario({
+//     name: 'Carlos',
+//     job: 'player'
+// }).then(console.log);
+
+// CRUD.actualizarUsuario(1, {
+//     name: 'Paula',
+//     job: 'be the best'
+// }).then(console.log);
+
+// CRUD.borrarUsuario(1).then(console.log);
+
+import { init } from './js/archivos-page';
+
+init();
